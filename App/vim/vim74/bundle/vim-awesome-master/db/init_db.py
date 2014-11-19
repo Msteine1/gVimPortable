58cfb0caa3585807afa07805090145410a3255de
Martin Steinert <Martin.Steinert@Koeln.de>
2014-11-19 20:40:56 +0100

    Weitere Plugins und SAS Syntax

---
+++ App/vim/vim74/bundle/vim-awesome-master/db/init_db.py
@@ -0,0 +1,17 @@
+import db.github_repos
+import db.plugins
+import db.submitted_plugins
+import db.tags
+
+
+def ensure_tables_and_indices():
+    db.plugins.ensure_table()
+    db.tags.ensure_table()
+    db.github_repos.PluginGithubRepos.ensure_table()
+    db.github_repos.DotfilesGithubRepos.ensure_table()
+    db.submitted_plugins.ensure_table()
+
+
+if __name__ == '__main__':
+    db.util.ensure_db('vim_awesome')
+    ensure_tables_and_indices()
